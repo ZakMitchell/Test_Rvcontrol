56509ef899abcf9f75659f78257eaed30a05719f
ZakMitchell <zakmit@ceh.ac.uk>
2019-04-15 09:20:07 +0100

    Initial code (1st commit)
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   Main script.R
    	new file:   Test1.Rproj
    
     Untracked files:
    	.gitignore

---
+++ Main script.R
@@ -0,0 +1,7 @@
+### Create Data ###
+
+ID <- seq(1,20)
+Treat <- rep(c("A", "B", "C", "D"), each = 5)
+Data1 <- rnorm(20, 5.6)
+Data2 <- seq(103.64, 70.02, length.out = 20)
+Data3 <- rnorm(20, 26.7)
