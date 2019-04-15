06853dc3fa4844bdf5c4d943fd24da4178efccb2
ZakMitchell <zak.mitchell@me.com>
2019-04-15 09:39:12 +0100

    Created data frame and skeleton for stats

--- Main script.R
+++ Main script.R
@@ -1,7 +1,13 @@
-### Create Data ###
-
+# Create Data
 ID <- seq(1,20)
 Treat <- rep(c("A", "B", "C", "D"), each = 5)
 Data1 <- rnorm(20, 5.6)
 Data2 <- seq(103.64, 70.02, length.out = 20)
 Data3 <- rnorm(20, 26.7)
+
+# Create data frame
+AllData <- data.frame(ID = ID, Treatment = Treat, Growth = Data1, Water = Data2, Temperature = Data3)
+
+  
+# Carry out statistics
+Test1 <- anova()
