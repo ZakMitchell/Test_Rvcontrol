9a410af5ca3bf85e6fe9b63599f75ccdb2712248
ZakMitchell <zak.mitchell@me.com>
2019-05-29 10:39:46 +0100

    Modified main script to create larger datasets

--- Main script.R
+++ Main script.R
@@ -1,9 +1,9 @@
 # Create Data
 ID <- seq(1,20)
-Treat <- rep(c("A", "B", "C", "D"), each = 5)
-Data1 <- rnorm(20, 5.6)
-Data2 <- seq(103.64, 70.02, length.out = 20)
-Data3 <- rnorm(20, 26.7)
+Treat <- rep(c("A", "B", "C", "D", "E"), each = 20)
+Data1 <- rnorm(100, 5.6)
+Data2 <- rep(seq(103.64, 70.02, length.out = 20), 5)
+Data3 <- rnorm(100, 26.7)
 
 # Create data frame
 AllData <- data.frame(ID = ID, Treatment = Treat, Growth = Data1, Water = Data2, Temperature = Data3)
